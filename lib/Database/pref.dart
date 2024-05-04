b7579fd071107b69b528594c1f6351d3816f44fd
DELL <rabeamarwan2@gmail.com>
2024-05-04 18:07:38 +0200

    profile 50%

--- lib/Database/pref.dart
+++ lib/Database/pref.dart
@@ -8,43 +8,66 @@ class SharedPreferenceHelper {
   static String userEmailKey = "USEREMAILKEY";
 
   static String userWalletKey = "USERWALLETKEY";
+  static String userProfileKey = "USEPROFILEKEY";
 
+
+////////// save athe keys\\\\\\\
+///////// saveUserId\\\\\\\\\\\\
   Future<bool> saveUserId(String getUserId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setString(userIdKey, getUserId);
   }
-
+///////////////saveUserName\\\\\\\\\\\\\\
   Future<bool> saveUserName(String getUserName) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setString(userNameKey, getUserName);
   }
-
+//////////////// saveUserEmail\\\\\\\\\\\\\\\\\\\
   Future<bool> saveUserEmail(String getUserEmail) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     return prefs.setString(userEmailKey, getUserEmail);
   }
-
+/////////////////saveUserWallet\\\\\\\\\\\\\\\\\\\
   Future<bool> saveUserWallet(String getUserWallet) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     return prefs.setString(userWalletKey, getUserWallet);
   }
+ ///////////// saveUserProfile\\\\\\\\\\\\\\\\\
+   Future<bool> saveUserProfile(String getUserProfile) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
 
+    return prefs.setString(userProfileKey, getUserProfile );
+  }
+
+  ///////////////////////gets\\\\\\\\\\\\\\
+  ///\\\\\\\\\\\\\\\\\\\////\/\/\//\\/\\/
+////////////////getUserId\\\\\\\\\\\\\\\\
   Future<String?> getUserId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(userIdKey);
   }
+ /////////////////  getUserName\\\\\\\\\\\\\\
     Future<String?> getUserName() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(userNameKey);
   }
+ /////////////////// getUserEmail\\\\\\\\\\\\\\\\\\
     Future<String?> getUserEmail() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(userEmailKey);
   }
+  ////////////////////getUserWallet\\\\\\\\\\\
     Future<String?> getUserWallet() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(userWalletKey);
   }
+  /////////////////getUserProfile\\\\\\\\\\\\\\\\\
+    Future<String?> getUserProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userProfileKey);
+  }
+
+
 }
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -10,6 +11,297 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+body:
+ Container(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        padding:
+                            EdgeInsets.only(top: 45.0, left: 20.0, right: 20.0),
+                        height: MediaQuery.of(context).size.height / 4.3,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.vertical(
+                                bottom: Radius.elliptical(
+                                    MediaQuery.of(context).size.width, 105.0))),
+                      ),
+
+Center(
+                        child: Container(
+                          margin: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height / 6.5),
+                          child: Material(
+                            elevation: 10.0,
+                            borderRadius: BorderRadius.circular(60),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(60),
+                              child:Image.asset(
+                                              "images/be2logo.png",
+                                              height: 120,
+                                              width: 120,
+                                              fit: BoxFit.fill,
+                                            )
+                                             ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 70.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Marwan",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                fontSize: 29.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ///////////////name field\\\\\\\\\\\\\\\\\
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 2.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Name",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  "name",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+///////////////////////////email feild\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 2.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Email",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  "email",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+              SizedBox(
+                    height: 30.0,
+                  ),
+                  ////////////////////trems and condition\\\\\\\\\\\\\\\\\
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 2.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.description,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Terms and Condition",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ///////////////////////delete account\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 2.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.delete,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Delete Account",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ///////// ///////////////////////LOg out\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 2.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.logout,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "LogOut",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
   }
 }
+
